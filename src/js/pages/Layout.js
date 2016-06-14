6ca8ae1fb31b98c3d0fa108b240534bbd78f1b51
qrp0002 <qrp0002@auburn.edu>
2016-06-14 21:30:34 +0000

    Added Routes and History

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router";
 
 export default class Layout extends React.Component {
+	navigate() {
+		console.log(this.props);
+		this.props.history.push(null, "/");
+	}
+
 	render() {
 		return (
+			<div>
 			<h1>KillerNews.net</h1>
+			{this.props.children}
+			<Link to="archives"> <button class="btn btn-success" onClick={this.navigate.bind(this)}> Archives </button></Link>
+			<Link to="settings"> <button class="btn btn-success" onClick={this.navigate.bind(this)}> Settings </button></Link>
+			<Link to="featured"> <button class="btn btn-success" onClick={this.navigate.bind(this)}> Featured </button></Link>
+			</div>
 		)
 	}
 }
