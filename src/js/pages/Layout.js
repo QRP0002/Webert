17454b725916f820b8f8adcd1c7245b4610f437b
qrp0002 <qrp0002@auburn.edu>
2016-06-15 15:20:54 +0000

    No more static html. Added collapse nav bar.

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,24 +1,37 @@
 import React from "react";
 import { Link } from "react-router";
 
+import Footer from "../components/layout/Footer";
+import Nav from "../components/layout/Nav";
+
 export default class Layout extends React.Component {
-	navigate() {
-		console.log(this.props);
-		this.props.history.push(null, "/");
-	}
+  render() {
+    const { location } = this.props;
+    const containerStyle = {
+      marginTop: "60px"
+    };
+    console.log("layout");
+    return (
+      <div>
 
+        <Nav location={location} />
 
+        <div class="container" style={containerStyle}>
+          <div class="row">
+            <div class="col-lg-12">
+            	<h1>KillerNews.net</h1>
+            	<div class="col-lg-12">
+        	  		<div class="well text-center">
+          				Ad spot goes here
+        			</div>
+      		   </div>
+            	{this.props.children}
+            </div>
+          </div>
+          <Footer/>
+        </div>
+      </div>
 
-	render() {
-		const { history } = this.props;
-		return (
-			<div>
-			<h1>KillerNews.net</h1>
-			{this.props.children}
-			<Link to="archives" activeClassName="archivesActive"> <button class="btn btn-success" onClick={this.navigate.bind(this)}> Archives </button></Link>
-			<Link to="settings"> <button class="btn btn-success" onClick={this.navigate.bind(this)}> Settings </button></Link>
-			<Link to="featured"> <button class="btn btn-success" onClick={this.navigate.bind(this)}> Featured </button></Link>
-			</div>
-		)
-	}
+    );
+  }
 }
